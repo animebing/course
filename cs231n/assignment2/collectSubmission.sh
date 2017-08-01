289c8713db0d4425f63b066ab48a6edda8525672
animebing <yanbing_dong@hotmail.com>
2017-08-02 00:01:05 +0800

    remove cs231n directory

--- cs231n/assignment2/collectSubmission.sh
+++
@@ -1,2 +0,0 @@
-rm -f assignment2.zip
-zip -r assignment2.zip . -x "*.git*" "cifar10/*" "*cs231n/datasets*" "*.ipynb_checkpoints*" "*README.md" "*collectSubmission.sh" "*requirements.txt" ".env/*" "*.pyc" "*cs231n/build/*"
